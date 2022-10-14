d5147de8a716b3b6e86b53b8d081599e038c69eb
Александр Анисимов <sanisimov@192.168.1.101>
2022-10-14 14:39:26 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Fragment, useState } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
 function App() {
+  const [cartIsShown, setCartisShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartisShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartisShown(false);
+  };
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
